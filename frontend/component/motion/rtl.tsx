8880177b3c633e00aff8c843e69d59f8e68f31f7
pravehsapkota <praveshsapkota2@gmail.com>
2022-11-21 02:43:08 -0800

    added intersection observer created LTR and RTL components

---
+++ frontend/component/motion/rtl.tsx
@@ -0,0 +1,15 @@
+import { motion } from "framer-motion"
+
+const LTR = () => {
+
+    <div>
+        <motion.div
+            animate={{
+                x: 0,
+                y: 0,
+                scale: 1,
+                rotate: 0,
+            }}
+        />
+    </div>
+}
